e8ca2d98aa74fa8d9aa3d49790de0409f14427d5
royun0307 <109058557+royun0307@users.noreply.github.com>
2025-03-31 20:46:29 +0900

    ⚡️ Add: “특성 추가”

--- Assets/Scripts/Entity/Traits/TraitData.cs
+++ Assets/Scripts/Entity/Traits/TraitData.cs
@@ -9,4 +9,11 @@ public class TraitData : ScriptableObject
     public StatType affectedStat;
     public float valueModifier;      // 고정 증가/감소
     public float multiplierModifier; // 배율 증가/감소
+
+    public TraitData Clone()
+    {
+        return new TraitData { 
+            traitName = this.traitName, description = this.description, affectedStat = this.affectedStat,
+            valueModifier = this.valueModifier, multiplierModifier = this.multiplierModifier};
+    }
 }
