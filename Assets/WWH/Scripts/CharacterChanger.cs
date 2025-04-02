8e2786ede2cfea8c5ec2d1a8c8a8161f06bfe0e1
woomay0608 <woohuck0608@naver.com>
2025-04-02 20:23:35 +0900

    👷 Chore: 전투 수정

--- Assets/WWH/Scripts/CharacterChanger.cs
+++ Assets/WWH/Scripts/CharacterChanger.cs
@@ -24,8 +24,11 @@ public class CharacterChanger : MonoBehaviour
         for (int i = 0;i < GameManager.instance.friendlyCharacterList.Count; i++)
         {
             transform.GetChild(i).gameObject.SetActive(true);
+
             BattleCharacterList.Add(transform.GetChild(i).GetComponent<Character>());
-            BattleCharacterList[i].info.job = GameManager.instance.friendlyCharacterList[i].info.job;
+            BattleCharacterList[i].info = GameManager.instance.friendlyCharacterList[i].info;
+            BattleCharacterList[i].GetComponent<Character>().stat = GameManager.instance.friendlyCharacterList[i].stat;
+
             BattleCharacterList[i].GetComponent<Player>().playerType = GameManager.instance.friendlyCharacterList[i].GetComponent<Player>().playerType;
         }
 
