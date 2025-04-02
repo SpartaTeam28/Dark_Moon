4daedae601661ffd73eb74ed835205a1da4758a9
royun0307 <109058557+royun0307@users.noreply.github.com>
2025-04-02 21:12:49 +0900

    🐛 Fix: “배경 오더 인 레이어 수정”

--- Assets/WWH/Scripts/CharacterChanger.cs
+++ Assets/WWH/Scripts/CharacterChanger.cs
@@ -23,11 +23,12 @@ public class CharacterChanger : MonoBehaviour
 
         for (int i = 0;i < GameManager.instance.friendlyCharacterList.Count; i++)
         {
-            transform.GetChild(i).gameObject.SetActive(true);
+            //transform.GetChild(i).gameObject.SetActive(true);
 
             BattleCharacterList.Add(transform.GetChild(i).GetComponent<Character>());
-            BattleCharacterList[i].info = GameManager.instance.friendlyCharacterList[i].info;
-            BattleCharacterList[i].GetComponent<Character>().stat = GameManager.instance.friendlyCharacterList[i].stat;
+            GameManager.instance.friendlyCharacterList[i].transform.position = BattleCharacterList[i].transform.position;
+            //BattleCharacterList[i].info = GameManager.instance.friendlyCharacterList[i].info;
+            //BattleCharacterList[i].GetComponent<Character>().stat = GameManager.instance.friendlyCharacterList[i].stat;
 
             BattleCharacterList[i].GetComponent<Player>().playerType = GameManager.instance.friendlyCharacterList[i].GetComponent<Player>().playerType;
         }
