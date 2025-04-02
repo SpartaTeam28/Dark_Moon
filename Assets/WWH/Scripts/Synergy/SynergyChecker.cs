03a940170e4f2d1f48dc88850bb76594cde74af9
woomay0608 <woohuck0608@naver.com>
2025-04-02 19:26:50 +0900

    👷 Chore: 4명이 아닐시 생기는 오류 수정

--- Assets/WWH/Scripts/Synergy/SynergyChecker.cs
+++ Assets/WWH/Scripts/Synergy/SynergyChecker.cs
@@ -85,7 +85,7 @@ public class SynergyChecker : MonoBehaviour
         //µµ»ç , °Ë°´, À¯»ý, ½º´Ô, »ç³É²Û, ±â»ý
         int number = SynergyCheaker.Where(element => element == true).Count();
         Debug.Log(number);
-        if(number == 1)
+        if(number == 1 && GameManager.instance.friendlyCharacterList.Count >1)
         {
             for (int i = 0; i< GameManager.instance.friendlyCharacterList.Count; i++) 
             {
